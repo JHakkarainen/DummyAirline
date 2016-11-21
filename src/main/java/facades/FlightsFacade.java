ccfb786bca116adbf89045e6a88ad061772edc4b
jarmo <jarmo@10.50.137.243>
2016-11-21 12:51:27 +0100

    Created Facade thingies for the Flights

---
+++ src/main/java/facades/FlightsFacade.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import entities.Flights;
+import java.sql.Date;
+
+/**
+ *
+ * @author jarmo
+ */
+public class FlightsFacade implements IFlights {
+
+    @Override
+    public Flights getAllFlights(String origin, String dest, Date date) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Flights getAllFlights(String origin, String dest) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Flights getWithDate(Date date) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Flights getWithOrigin(String origin, Date date) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+}
---
+++ src/main/java/facades/IFlights.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import entities.Flights;
+import java.sql.Date;
+
+/**
+ *
+ * @author jarmo
+ */
+public interface IFlights {
+    public Flights getAllFlights(String origin, String dest, Date date);
+    public Flights getAllFlights(String origin, String dest);
+    public Flights getWithDate(Date date);
+    public Flights getWithOrigin(String origin, Date date);
+}
